cb8099eb385f2ea8dd4f8815be47cb1c0ad33743
DESKTOP-8D7GF1E\shopc <cyj4262@naver.com>
2023-08-02 18:25:42 +0900

    #1.2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 @Module({
   imports: [],
-  controllers: [AppController], //url 가져오고 함수를 실행
-  providers: [AppService], //
+  controllers: [], //url 가져오고 함수를 실행
+  providers: [], //
 })
 export class AppModule {}
