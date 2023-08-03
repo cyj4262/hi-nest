af48eea32c95190406a373d9081184d015fa6187
DESKTOP-8D7GF1E\shopc <cyj4262@naver.com>
2023-08-03 15:35:33 +0900

    #3.4

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -49,4 +49,63 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe("deleteOne", () => {
+
+    it("deletes a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2000
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toEqual(beforeDelete - 1);
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it("should return a 404", () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
+
+  describe("create", () => {
+    it("should create a movie", () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2000
+      });
+      const AfterCreate = service.getAll().length;
+      console.log(AfterCreate, beforeCreate)
+      expect(AfterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe("update", () => {
+    it("should update a movie", () => {
+      service.create({
+        title: "Test Movie",
+        genres: ["test"],
+        year: 2000
+      });
+      service.update(1, {title:'Updated Test'});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('Updated Test');
+    });
+    it("should throw a NotFoundException", () => {
+      try {
+        service.update(999, {});
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+  });
 });
