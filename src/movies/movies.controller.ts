eedce648c0eda6c3c852add8f2f84e0da6196a11
DESKTOP-8D7GF1E\shopc <cyj4262@naver.com>
2023-08-03 11:51:35 +0900

    #2.4

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entites/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies') //url의 entry point를 컨트롤
 export class MoviesController {
@@ -13,21 +14,22 @@ export class MoviesController {
     }
 
     @Get(":id")
-    getOne(@Param('id') movieId: string): Movie {
+    getOne(@Param('id') movieId: number): Movie {
+        console.log(typeof movieId);
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
 
     @Delete(":id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
     @Patch(":id")
-    path(@Param("id") movieId: string, @Body() updateData) {
+    path(@Param("id") movieId: number, @Body() updateData) {
         return this.moviesService.update(movieId, updateData);
     }
 
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entites/movie.entity';
+import { CreateContextOptions } from 'vm';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Injectable()
 export class MoviesService {
@@ -8,24 +10,24 @@ export class MoviesService {
     getAll(): Movie[] {
         return this.movies;
     }
-    getOne(id: string): Movie {
-        const movie = this.movies.find(movie => movie.id === parseInt(id)); // 'parseInt()'는   '+id'와 같음
+    getOne(id: number): Movie {
+        const movie = this.movies.find(movie => movie.id === id); // 'parseInt()'는   '+id'와 같음
         if (!movie) {
             throw new NotFoundException(`Movie with ID: ${id} not found.`)
         }
         return movie;
     }
-    deleteOne(id: string) {
+    deleteOne(id: number) {
         this.getOne(id)
-        this.movies = this.movies.filter(movie => movie.id !== +id);
+        this.movies = this.movies.filter(movie => movie.id !== id);
     }
-    create(movieData) {
+    create(movieData: CreateMovieDto) {
         this.movies.push({
             id: this.movies.length + 1,
             ...movieData,
         })
     }
-    update(id: string, updateData) {
+    update(id: number, updateData) {
         const movie = this.getOne(id);
         this.deleteOne(id);
         this.movies.push({ ...movie, ...updateData });
