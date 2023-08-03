666336e5a0af21e702fadfe283b4e036615c6eee
DESKTOP-8D7GF1E\shopc <cyj4262@naver.com>
2023-08-03 10:22:34 +0900

    #2.1

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
+import { Controller, Get, Param, Post, Delete, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies') //url의 entry point를 컨트롤
 export class MoviesController {
@@ -7,23 +7,33 @@ export class MoviesController {
         return "This will return all movies";
     }
 
-    @Get("/:id")
+    @Get("search")
+    search(@Query("year") searchingYear: string){
+        return `We are searching for a movie made after: ${searchingYear}`;
+    }
+
+    @Get(":id")
     getOne(@Param('id') moviesid: string) {
         return `This will return one movie with the id: ${moviesid}`;
     }
 
     @Post()
-    create() {
-        return 'This will create a movie';
+    create(@Body() movieData) {
+        return movieData;
     }
 
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') moviesid: string) {
         return `This will delete a movie with the id: ${moviesid}`;
     }
-    @Patch("/:id")
-    path(@Param("id") moviesid: string) {
-        return `This will patch a movie with the id: ${moviesid}`;
+    @Patch(":id")
+    path(@Param("id") moviesid: string, @Body() updateData) {
+        return {
+            updateMovie: moviesid,
+            ...updateData,
+        };
     }
+
+    
 
 }
